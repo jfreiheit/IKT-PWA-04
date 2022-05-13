dcf81b59475fce64599f215ea44ef2d55442a36e
J. Freiheit <freiheit@htw-berlin.de>
2022-05-13 14:06:08 +0200

    vor IndexedDB

---
+++ public/src/js/feed.js
@@ -0,0 +1,46 @@
+let shareImageButton = document.querySelector('#share-image-button');
+let createPostArea = document.querySelector('#create-post');
+let closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+let sharedMomentsArea = document.querySelector('#shared-moments');
+
+function openCreatePostModal() {
+  createPostArea.style.visibility = 'visible';
+}
+
+function closeCreatePostModal() {
+  createPostArea.style.visibility = 'hidden';
+}
+
+shareImageButton.addEventListener('click', openCreatePostModal);
+
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function createCard() {
+  let cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  let cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/htw-gebaeude-h.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  let cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'Vor der HTW-Mensa';
+  cardTitle.appendChild(cardTitleTextElement);
+  let cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'HTW Berlin';
+  cardSupportingText.style.textAlign = 'center';
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      createCard();
+    });
